c5943e34208507e406462d5d6fe988b3b69950df
ValValeria <led2017l@gmail.com>
2021-10-07 17:06:04 +0300

    need fixes

--- shop/src/app/Components/slider-info-content/slider-info-content.component.ts
+++ shop/src/app/Components/slider-info-content/slider-info-content.component.ts
@@ -3,6 +3,12 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 import {HttpService} from '../../Services/Http.service';
 import {IAllCarouselResponse} from '../../interfaces/interfaces';
 import { AfterViewInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { image } from '../../Pages/slider-info-page/slider-info-page.component';
+import { from } from 'rxjs';
+import { auditTime, map, mergeMap } from 'rxjs/operators';
+import _ from 'lodash';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-slider-info-content',
@@ -10,21 +16,46 @@ import { AfterViewInit } from '@angular/core';
   styleUrls: ['./slider-info-content.component.scss']
 })
 export class SliderInfoContentComponent implements OnInit, AfterViewInit{
-  @Input() photos: string[] = [];
   @Input() type = '';
   @ViewChild('file', {read: ElementRef}) fileElement: ElementRef<HTMLInputElement>;
-  @Output() uploadFile = new EventEmitter<File>();
-  @Output() saveFiles = new EventEmitter<void>();
-  @Output() loadPrevImages = new EventEmitter<string[]>();
+  @Output() uploadFileEvent = new EventEmitter<void>();
+  private urlPattern = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+  public form: FormGroup;
+  public photosFile: image<File>[] = [];
+  public photos: image<string>[] = [];
+  public sub: Subscription[] = [];
 
   constructor(private snackBar: MatSnackBar,
-              private httpService: HttpService
-  ) { }
+    private httpService: HttpService,
+    private bd: FormBuilder
+  ) {
+    this.form = this.bd.group({
+      urls: this.bd.array([])
+    });
+  }
 
   ngOnInit(): void {
     this.httpService.get<IAllCarouselResponse>(`/api/carousel/${this.type}`)
-      .subscribe(v => {
-          this.loadPrevImages.emit(v.data.images);
+      .subscribe(async(v) => {
+        const data = v.data.images;
+        const validators = [Validators.required, Validators.min(10), Validators.max(40), Validators.pattern(this.urlPattern)];
+
+        this.photos.push(...v.data.images);
+
+        for (let i = 0; i < data.length; i++) {
+          const image = data[i];
+          const fileName = _.last(image.file.split('/')) ?? 'image.png';
+          const response = await fetch(image.file);
+          const blob = await response.blob();
+          const file = new File([blob], fileName, { type: blob.type, lastModified: Date.now() });
+
+          this.photosFile.push({ file, postUrl: image.postUrl });
+          this.photos.push(image);
+
+          const formControl = new FormControl(image.postUrl, validators);
+          this.formArray.push(formControl);
+          this.watchForUrlChanges(formControl, i)
+        }
       });
   }
 
@@ -36,24 +67,67 @@ export class SliderInfoContentComponent implements OnInit, AfterViewInit{
     this.fileElement.nativeElement.click();
   }
 
+  watchForUrlChanges(formControl: FormControl, index: number): void {
+     const sub: Subscription = formControl.valueChanges.pipe(auditTime(200))
+      .subscribe(v => {
+        const item1 = this.photosFile[index];
+        const item2 = this.photos[index];
+
+        item1.postUrl = item2.postUrl = v;
+      });
+
+     this.sub.push(sub);
+  }
+
   loadImage(): void{
-    const file = this.fileElement.nativeElement.files[0];
+    const files = this.fileElement.nativeElement.files;
+    const file: File = files[files.length - 1];
+    const url = URL.createObjectURL(file);
 
-    console.log(file)
-
-    if (file != null){
-      this.uploadFile.emit(file);
+    if (file != null) {
+      this.photos.push({ file: url, postUrl: '' });
+      this.photosFile.push({ file, postUrl: '' });
     } else {
       this.snackBar.open('Please, choose the file', 'Close');
     }
   }
 
-  handleSave(): void{
-    this.saveFiles.emit();
+  async handleSubmit(): Promise<void>{
+    if (this.form.valid) {
+      const url = `/api/sliders/download/${this.type}`;
+      const formData = new FormData();
+      const urls = this.photos.map(v => v.postUrl);
+      const blob = new Blob([JSON.stringify(urls)], {type: 'application/json'});
+
+      this.photosFile.forEach(v => {
+        formData.append(this.type, v.file, v.file.name);
+      });
+
+      formData.append('urls_list', blob, 'urls_list.json');
+
+      const response = await fetch(url, {
+        method: 'POST',
+        body: formData
+      });
+
+      if (response.ok) {
+        this.snackBar.open('Files is saved', 'Close');
+      }
+    } else {
+      this.snackBar.open('Invalid url', 'Close');
+    }
   }
 
   handleImageLoading(img: HTMLImageElement, src: string): void {
     img.src = src;
     img.onload = () => img.hidden = false;
+  }
+
+  preventEvent($event: Event) {
+    $event.preventDefault();
+  }
+
+  get formArray(): FormArray {
+    return this.form.get('urls') as FormArray;
   }
 }
--- shop/src/app/Pages/slider-info-page/slider-info-page.component.ts
+++ shop/src/app/Pages/slider-info-page/slider-info-page.component.ts
@@ -17,69 +17,9 @@ export type image<T> = { postUrl: string, file: T };
 export class SliderInfoPageComponent{
   public data: tuple = ['Отдельный товар', 'Главная', 'Продукты'];
   public pageType: tuple = ['product', 'home', 'products'];
-  public photos: {[prop in number]: image<string>[]} = {
-    0: [],
-    1: [],
-    2: []
-  };
-  public photosFile: {[prop in number]: image<File>[]} = {
-    0: [],
-    1: [],
-    2: []
-  };
-  public isEmpty = true;
 
   constructor(private snackBar: MatSnackBar,
               private httpService: HttpService
               ) {
-  }
-
-  uploadFile(title: string, $event: image<File>): void {
-    const index = this.pageType.indexOf(title);
-
-    if (index !== -1){
-      const url = URL.createObjectURL($event.file);
-
-      this.photos[index].push({ postUrl: $event.postUrl, file: url });
-      this.photosFile[index].push($event);
-      this.isEmpty = false;
-    }
-  }
-
-  async handleSubmit(): Promise<void>{
-    const formData = new FormData();
-    const response = await fetch('/api/sliders/download', {
-      method: 'POST',
-      body: formData
-    });
-    const filesText = _.flattenDeep(Object.values(this.photosFile));
-    const filesTextResult: string[] = await Promise.all(filesText.map(v => v.text()));
-
-    filesTextResult.forEach((v, index) => {
-      const file: File = filesText[index];
-      const pageForFileIndex = Object.values(this.photosFile).findIndex(value => value.includes(file));
-
-      if (pageForFileIndex !== -1){
-        const title = this.pageType[pageForFileIndex];
-        const blob = new Blob([v], { type: file.type })
-        formData.append(title, blob, file.name);
-      }
-    });
-
-    let message = '';
-
-    if (response.ok && response.status === 200){
-       const json: ISimpleResponse = await response.json();
-
-       if (json.status === 'ok'){
-          message = 'Your files has been uploaded successfully';
-       } else{
-          message = 'Your files has not been uploaded successfully. PLease, try again';
-       }
-    } else {
-       message = 'Some errors has occurred';
-    }
-
-    this.snackBar.open(message, 'Close');
   }
 }
