dba158412b0b92fa7e1d295a6a0bf4e76c996f35
ValValeria <led2017l@gmail.com>
2022-05-29 13:25:40 +0200

    fix: change card-small component

--- shop/src/app/Components/card-small/card-small.component.ts
+++ shop/src/app/Components/card-small/card-small.component.ts
@@ -1,53 +1,61 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-import { IAd } from '../../interfaces/interfaces';
-import { ImageLoading } from '../../Classes/ImageLoading';
-import { Router } from '@angular/router';
-import { URL_PATH } from 'src/app/app.component';
-import { HttpService } from 'src/app/services/http.service';
-import { UserService } from 'src/app/services/user.service';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {IAd} from '../../interfaces/interfaces';
+import {ImageLoading} from '../../Classes/ImageLoading';
+import {Router} from '@angular/router';
+import {URL_PATH} from 'src/app/app.component';
+import {HttpService} from 'src/app/services/http.service';
+import {UserService} from 'src/app/services/user.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
-    selector: 'app-card',
-    templateUrl: './card-small.component.html',
-    styleUrls: ['./card-small.component.scss']
+  selector: 'app-card',
+  templateUrl: './card-small.component.html',
+  styleUrls: ['./card-small.component.scss']
 })
-export class CardSmallComponent extends ImageLoading{
-    @Input() card: IAd;
-    @Input() showFull = true;
-    @ViewChild('img', {read: ElementRef}) public image: ElementRef;
-    @Input() showButton = true;
-    @Input() emptyCard = false;
+export class CardSmallComponent extends ImageLoading {
+  @Input() card: IAd;
+  @Input() showFull = true;
+  @Input() showButton = true;
+  @Input() emptyCard = false;
 
-    constructor(private router: Router,
-                private user: UserService,
-                private http: HttpService,
-                private snackBar: MatSnackBar){
-        super();
-    }
+  @ViewChild('img', {read: ElementRef}) public image: ElementRef;
 
-    buyItem(): void {
-        if (!this.user.is_auth) {
-            this.snackBar.open('Только авторизированные пользователи могут добавлять товар в корзину', 'Закрыть', {
-                duration: 5000
+  constructor(
+    private router: Router,
+    private user: UserService,
+    private http: HttpService,
+    private snackBar: MatSnackBar) {
+    super();
+  }
+
+  async handleClick(): Promise<void> {
+    await this.router.navigateByUrl(`/product/${this.card.id}`);
+  }
+
+  buyItem($event: Event): void {
+    $event.preventDefault();
+
+    if (!this.user.is_auth) {
+      this.snackBar.open('Только авторизированные пользователи могут добавлять товар в корзину', 'Закрыть', {
+        duration: 5000
+      });
+    } else {
+      this.http.get<{ messages: string[], data: string[], status: string }>(`${URL_PATH}api/addorder?product_id=${this.card.id}&count=${1}`)
+        .subscribe(v => {
+          if (v.status === 'ok') {
+            this.snackBar.open('Товар добавлен в корзину', 'Закрыть', {
+              duration: 5000
             });
-        } else {
-            this.http.get<{ messages: string[], data: string[], status: string }>(`${URL_PATH}api/addorder?product_id=${this.card.id}&count=${1}`)
-            .subscribe(v => {
-                if (v.status === 'ok') {
-                    this.snackBar.open('Товар добавлен в корзину', 'Закрыть', {
-                        duration: 5000
-                    });
-                }
-             });
-        }
+          }
+        });
     }
+  }
 
-    goToCat(): void{
-        this.router.navigate(['category', this.card.category]).then(r => console.log('navigated'));
-    }
+  goToCat(): void {
+    this.router.navigate(['category', this.card.category]).then(r => console.log('navigated'));
+  }
 
-    get styles(): any{
-        return {height: this.showFull ? '500px' : '400px'};
-    }
+  get styles(): any {
+    return {height: this.showFull ? '500px' : '400px'};
+  }
 }
--- shop/src/app/Components/card-small/card-small.module.ts
+++ shop/src/app/Components/card-small/card-small.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import {CardSmallComponent} from './card-small.component';
 import {SharedModule} from '../../shared/shared.module';
-import {RouterModule} from '@angular/router';
 import {ButtonModule} from 'primeng/button';
 import {SliceStringPipe} from '../../pipes/slice-string.pipe';
 
 @NgModule({
   declarations: [CardSmallComponent, SliceStringPipe],
-  imports: [SharedModule, RouterModule, ButtonModule],
+  imports: [SharedModule, RouterModule.forChild([]), ButtonModule],
   exports: [CardSmallComponent]
 })
 export class CardSmallModule {
